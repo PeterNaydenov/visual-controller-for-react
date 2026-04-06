c72e3ba8d4de9d5ed6d9eb8c30655fdfe83d58b6
Peter Naydenov <peter.naydenov@gmail.com>
2026-04-06 11:14:10 +0300

    Test: Using jsdom instead of happy-dom

--- test/setup.js
+++ test/setup.js
@@ -1,12 +1,5 @@
-import { beforeAll, afterEach } from 'vitest'
+import { afterEach } from 'vitest'
 import { cleanup } from '@testing-library/react'
-import { Window } from 'happy-dom'
-
-beforeAll(() => {
-  const window = new Window()
-  global.window = window
-  global.document = window.document
-})
 
 afterEach(() => {
   cleanup()
