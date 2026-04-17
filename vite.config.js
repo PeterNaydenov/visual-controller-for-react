5c85956f7fdbf19905c7a4e98e379336aa332f56
Peter Naydenov <peter.naydenov@gmail.com>
2026-04-17 12:05:11 +0300

    vite config update

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dts from 'vite-plugin-dts'
 import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts({ entry: resolve(__dirname, 'src/main.js'), cleanVueFileName: true, outDir: 'dist' })],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/main.js'),
