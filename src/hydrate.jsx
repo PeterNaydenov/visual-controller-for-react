1c1c9564136dce9be6cfcf443977962ba8f3f13e
Peter Naydenov <peter.naydenov@gmail.com>
2026-04-17 12:04:35 +0300

    types: jsdoc for jsx files

--- src/hydrate.jsx
+++ src/hydrate.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 import { hydrateRoot } from 'react-dom/client'
 
+/**
+ * Hydrate a React component.
+ * @param {HTMLElement} node - Element to hydrate into
+ * @param {React.ComponentType} Comp - React component to hydrate
+ * @param {Object} [props={}] - Props to pass to the component
+ * @returns {import('react-dom/client').Root} - Hydrated React root
+ */
 function hydrate ( node, Comp, props={} ) {
-    return hydrateRoot ( node, <Comp {...props} /> )
+    return hydrateRoot ( node, React.createElement(Comp, props) )
 } // hydrate func.
 
 
--- src/setupReactElement.jsx
+++ src/setupReactElement.jsx
@@ -3,6 +3,13 @@ import React, { useEffect } from "react";
 
 
 
+/**
+ * Create a React element with a wrapper component that waits for the loadTask promise to be resolved.
+ * @param {Object} loadTask - An ask-for-promise object that should be resolved when the React app is ready.
+ * @param {React.ComponentType} Fn - The React component to be wrapped.
+ * @param {Object} props - Props of the React component.
+ * @returns {React.ReactElement} A React element with the wrapped component.
+ */
 function SetupReactElement ( loadTask, Fn, props ) {
 
     function dummy ( props ) {
@@ -12,7 +19,8 @@ function SetupReactElement ( loadTask, Fn, props ) {
                     <>
                         {el}
                     </>
-        )}
+        )
+    }
 
    const el = React.createElement ( Fn, props );
    return     React.createElement ( dummy, {el});
