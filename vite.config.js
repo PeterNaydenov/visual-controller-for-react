592aea2efa4c1fec98a55eaf65807b0aa6de9b00
Peter Naydenov <peter.naydenov@gmail.com>
2026-04-17 11:44:39 +0300

    Building script for different packages: es,commonjs, umd

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,36 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/main.js'),
+      name: 'VisualControllerForReact',
+      formats: ['es', 'cjs', 'umd'],
+      fileName: (format) => {
+        switch (format) {
+          case 'es':
+            return 'visual-controller-for-react.esm.js'
+          case 'cjs':
+            return 'visual-controller-for-react.cjs'
+          case 'umd':
+            return 'visual-controller-for-react.umd.js'
+          default:
+            return 'visual-controller-for-react.js'
+        }
+      }
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM'
+        }
+      }
+    }
+  }
 })
